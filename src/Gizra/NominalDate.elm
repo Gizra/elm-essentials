a45d7912450343614e65e7b71c06da0da7a3144e
Ryan Rempel <rgrempel@gmail.com>
2017-10-02 14:05:50 -0500

    Add NominalDateRange.

--- src/Gizra/NominalDate.elm
+++ src/Gizra/NominalDate.elm
@@ -1,16 +1,25 @@
 module Gizra.NominalDate
     exposing
-        ( decodeYYYYMMDD
+        ( decodeDrupalRange
+        , decodeYYYYMMDD
+        , encodeDrupalRange
+        , encodeYYYYMMDD
         , formatYYYYMMDD
         , formatMMDDYYYY
         , fromLocalDateTime
         , NominalDate
+        , NominalDateRange
         )
 
 {-| Some utilities for dealing with "pure" dates that have no time or
 time zone information.
 
-@docs NominalDate, decodeYYYYMMDD, formatYYYYMMDD, formatMMDDYYYY, fromLocalDateTime
+@docs NominalDate, decodeYYYYMMDD, encodeYYYYMMDD, formatYYYYMMDD, formatMMDDYYYY, fromLocalDateTime
+
+
+## Ranges
+
+@docs NominalDateRange, decodeDrupalRange, encodeDrupalRange
 
 -}
 
@@ -19,6 +28,8 @@ import Date.Extra.Facts exposing (monthNumberFromMonth)
 import Gizra.String exposing (addLeadingZero, addLeadingZeroes)
 import Json.Decode exposing (Decoder, andThen, string)
 import Json.Decode.Extra exposing (fromResult)
+import Json.Decode.Pipeline exposing (decode, required)
+import Json.Encode exposing (Value, object)
 import Time.Date exposing (year, month, day, fromISO8601)
 
 
@@ -31,6 +42,17 @@ This is basically to avoid confusion between `Time.Date.Date` and the
 -}
 type alias NominalDate =
     Time.Date.Date
+
+
+{-| A range of nominal dates, with a start and end.
+
+Both the start and end date are included in the range.
+
+-}
+type alias NominalDateRange =
+    { start : NominalDate
+    , end : NominalDate
+    }
 
 
 {-| Convert a nominal date to formatted string.
@@ -78,3 +100,56 @@ fromLocalDateTime date =
 decodeYYYYMMDD : Decoder NominalDate
 decodeYYYYMMDD =
     andThen (fromResult << fromISO8601) string
+
+
+{-| Encode nominal date to string of the form "2017-02-20".
+
+    import Json.Encode exposing (encode)
+
+    encodeYYYYMMDD (date 2017 2 20)
+        |> encode 0 --> "\"2017-02-20\""
+
+-}
+encodeYYYYMMDD : NominalDate -> Value
+encodeYYYYMMDD =
+    Json.Encode.string << formatYYYYMMDD
+
+
+{-| Given a decoder, Decodes a range as Drupal sends it, with a `value` and `value2`.
+
+    """
+        { "value": "2017-07-21"
+        , "value2": "2017-07-23"
+        }
+    """
+        |> decodeString (decodeDrupalRange decodeYYYYMMDD)
+    --> Ok
+            { start = date 2017 07 21
+            , end = date 2017 07 23
+            }
+
+-}
+decodeDrupalRange : Decoder NominalDate -> Decoder NominalDateRange
+decodeDrupalRange decoder =
+    decode NominalDateRange
+        |> required "value" decoder
+        |> required "value2" decoder
+
+
+{-| Given an encoder, encodes a range as Drupal expects it, with a `value` and `value2`.
+
+    { start = date 2017 07 21
+    , end = date 2017 07 23
+    }
+        |> encodeDrupalRange encodeYYYYMMDD
+        |> encode 0
+    --> """{"value":"2017-07-21","value2":"2017-07-23"}"""
+
+-}
+encodeDrupalRange : (NominalDate -> Value) -> NominalDateRange -> Value
+encodeDrupalRange encoder range =
+    object <|
+        ([ ( "value", encoder range.start )
+         , ( "value2", encoder range.end )
+         ]
+        )
