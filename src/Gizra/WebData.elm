4e1ec64cc708ac5dc2e3ea03fc27ad0b8fdc2a3d
Ryan Rempel <rgrempel@gmail.com>
2018-03-07 14:17:31 -0600

    Add `whenNotAsked` and `resetError` to Gizra.WebData.

--- src/Gizra/WebData.elm
+++ src/Gizra/WebData.elm
@@ -1,8 +1,8 @@
-module Gizra.WebData exposing (sendWithHandler)
+module Gizra.WebData exposing (resetError, sendWithHandler, whenNotAsked)
 
 {-| Some functions for working with WebData.
 
-@docs sendWithHandler
+@docs sendWithHandler, whenNotAsked, resetError
 
 -}
 
@@ -55,3 +55,63 @@ sendWithHandler decoder tagger builder =
         |> HttpBuilder.toTask
         |> RemoteData.asCmd
         |> Cmd.map tagger
+
+
+{-| Given a possible `msg`, returns `Just msg` if the `RemoteData` is
+`NotAsked`. Otherwise, returns `Nothing`.
+
+The use case for this is where you want to start some process (e.g.
+an HTTP GET) if we have no data, no request in progress, and no error.
+
+See `resetError` to return the `RemoteData` from an error state back
+to `NotAsked`, in cases where (for instance) the user has acknowledged
+an error and wishes to retry the request.
+
+    import Http exposing (Error(Timeout))
+    import Fixtures exposing (Msg(DoThis))
+    import RemoteData exposing (..)
+
+    whenNotAsked DoThis NotAsked --> Just DoThis
+
+    whenNotAsked DoThis (Success ()) --> Nothing
+
+    whenNotAsked DoThis Loading --> Nothing
+
+    whenNotAsked DoThis (Failure Timeout) --> Nothing
+
+-}
+whenNotAsked : msg -> RemoteData e a -> Maybe msg
+whenNotAsked msg data =
+    case data of
+        NotAsked ->
+            Just msg
+
+        _ ->
+            Nothing
+
+
+{-| If the `RemoteData` is in a `Failure` state, then return `NotAsked`.
+Otherwise, returns the `RemoteData` unchanged.
+
+The use case for this is to deal with situations where the user has
+acknowledged the error (perhaps clicked on a "Retry" button), and you'd now
+like to engage some code that will retry the request if it is `NotAsked`. (See
+`whenNotAsked`, for instance).
+
+    resetError NotAsked --> NotAsked
+
+    resetError (Success ()) --> Success ()
+
+    resetError Loading --> Loading
+
+    resetError (Failure Timeout) --> NotAsked
+
+-}
+resetError : RemoteData e a -> RemoteData e a
+resetError data =
+    case data of
+        Failure _ ->
+            NotAsked
+
+        _ ->
+            data
---
+++ tests/Fixtures.elm
@@ -0,0 +1,6 @@
+module Fixtures exposing (..)
+
+
+type Msg
+    = DoThis
+    | DoThat
